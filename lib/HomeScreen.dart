8526f185c5d73114b907d065460da38d408237be
Harshit Sonawala <hssonawala@gmail.com>
2022-01-30 01:58:58 +0530

    added reusable MainOptionButton

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -6,11 +6,40 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const optionsList = <Map>[];
+    const optionsList = <Map>[
+      {
+        'optionId': 1,
+        'optionTitle': 'Translate Sign Language',
+        'optionIcon': Icons.translate_rounded,
+        'optionColor': Colors.red,
+        'optionNavigate': 'navigate',
+      },
+      {
+        'optionId': 2,
+        'optionTitle': 'Recognise Text (OCR)',
+        'optionIcon': Icons.center_focus_strong_rounded,
+        'optionColor': Colors.blue,
+        'optionNavigate': 'navigate',
+      },
+      {
+        'optionId': 3,
+        'optionTitle': 'Read Text Out Loud',
+        'optionIcon': Icons.record_voice_over,
+        'optionColor': Colors.green,
+        'optionNavigate': 'navigate',
+      },
+      {
+        'optionId': 4,
+        'optionTitle': 'ASL Guide',
+        'optionIcon': Icons.my_library_books,
+        'optionColor': Colors.deepPurple,
+        'optionNavigate': 'navigate',
+      },
+    ];
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Sign Language Translator'),
-      // ),
+      appBar: AppBar(
+        title: const Text('Sign Language Translator'),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -22,7 +51,7 @@ class HomeScreen extends StatelessWidget {
                 Text(
                   'Welcome, User!',
                   style: TextStyle(
-                    color: Colors.cyan,
+                    color: Colors.deepPurple,
                     fontSize: 30.0,
                   ),
                 ),
@@ -47,11 +76,13 @@ class HomeScreen extends StatelessWidget {
                     mainAxisSpacing: 10.0,
                     crossAxisSpacing: 10.0,
                   ),
-                  itemCount: 4,
+                  itemCount: optionsList.length,
                   itemBuilder: (BuildContext context, int index) {
                     return MainOptionButton(
-                      passedTitle: 'Option $index',
-                    );
+                        passedTitle: optionsList[index]['optionTitle'],
+                        passedIcon: optionsList[index]['optionIcon'],
+                        passedColor: optionsList[index]['optionColor'],
+                        passedNavigate: optionsList[index]['optionNavigate']);
                   }),
             ),
           ),
--- lib/reusableWidgets/MainOptionButton.dart
+++ lib/reusableWidgets/MainOptionButton.dart
@@ -2,34 +2,46 @@ import 'package:flutter/material.dart';
 
 class MainOptionButton extends StatelessWidget {
   final String passedTitle;
+  final IconData passedIcon;
+  final Color passedColor;
+  final String passedNavigate;
 
-  const MainOptionButton({
-    Key? key,
-    required this.passedTitle,
-  }) : super(key: key);
+  const MainOptionButton(
+      {Key? key,
+      required this.passedTitle,
+      required this.passedIcon,
+      required this.passedColor,
+      required this.passedNavigate})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      // borderRadius: BorderRadius.circular(20.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
       elevation: 3.0,
-      color: Colors.cyan,
+      color: passedColor,
       child: InkWell(
         onTap: () => {},
         child: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Icon(
-              Icons.center_focus_strong_outlined,
+            Icon(
+              passedIcon,
               color: Colors.white,
+              size: 40,
             ),
             const SizedBox(
               height: 10,
             ),
             Text(
               passedTitle,
-              style: const TextStyle(color: Colors.white),
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 14.0,
+              ),
             ),
           ],
         )),
